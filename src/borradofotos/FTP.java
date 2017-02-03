8dd077936d32985c407ca66f245a1bebd4596d39
SaraAlamillo <saraalamillo93@gmail.com>
2017-02-03 13:22:54 +0100

    Añadidos métodos para la descarga de ficheros

--- src/borradofotos/FTP.java
+++ src/borradofotos/FTP.java
@@ -1,6 +1,10 @@
 package borradofotos;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.*;
 import org.apache.commons.net.ftp.*;
 
@@ -187,5 +191,81 @@ public class FTP {
         }
 
         return result;
+    }
+
+    public void downloadDirectory(String parentDir, String currentDir, String saveDir) throws IOException {
+        String dirToList = parentDir;
+        if (!currentDir.equals("")) {
+            dirToList += "/" + currentDir;
+        }
+
+        FTPFile[] subFiles = this.connect.listFiles(dirToList);
+
+        if (subFiles != null && subFiles.length > 0) {
+            for (FTPFile aFile : subFiles) {
+                String currentFileName = aFile.getName();
+                if (currentFileName.equals(".") || currentFileName.equals("..")) {
+                    // skip parent directory and the directory itself
+                    continue;
+                }
+                String filePath = parentDir + "/" + currentDir + "/"
+                        + currentFileName;
+                if (currentDir.equals("")) {
+                    filePath = parentDir + "/" + currentFileName;
+                }
+
+                String newDirPath = saveDir + parentDir + File.separator
+                        + currentDir + File.separator + currentFileName;
+                if (currentDir.equals("")) {
+                    newDirPath = saveDir + parentDir + File.separator
+                            + currentFileName;
+                }
+
+                if (aFile.isDirectory()) {
+                    // create the directory in saveDir
+                    File newDir = new File(newDirPath);
+                    boolean created = newDir.mkdirs();
+                    if (created) {
+                        System.out.println("CREATED the directory: " + newDirPath);
+                    } else {
+                        System.out.println("COULD NOT create the directory: " + newDirPath);
+                    }
+
+                    // download the sub directory
+                    downloadDirectory(dirToList, currentFileName, saveDir);
+                } else {
+                    // download the file
+                    boolean success = downloadSingleFile(filePath, newDirPath);
+                    if (success) {
+                        System.out.println("DOWNLOADED the file: " + filePath);
+                    } else {
+                        System.out.println("COULD NOT download the file: "
+                                + filePath);
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean downloadSingleFile(String remoteFilePath, String savePath) throws IOException {
+        File downloadFile = new File(savePath);
+
+        File parentDir = downloadFile.getParentFile();
+        if (!parentDir.exists()) {
+            parentDir.mkdir();
+        }
+
+        OutputStream outputStream = new BufferedOutputStream(
+                new FileOutputStream(downloadFile));
+        try {
+            this.connect.setFileType(FTPClient.BINARY_FILE_TYPE);
+            return this.connect.retrieveFile(remoteFilePath, outputStream);
+        } catch (IOException ex) {
+            throw ex;
+        } finally {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
     }
 }
